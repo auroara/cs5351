e84ab36650349d24a08d6f18f5cacc4b8e785a48
wangbo <wangbo@DESKTOP-JP8QPEM.mshome.net>
2025-03-25 14:49:43 +0800

    initial commit

--- src/main/java/Samples/FloorSquareRoot.java
+++ src/main/java/Samples/FloorSquareRoot.java
@@ -3,7 +3,6 @@ package Samples;
 public class FloorSquareRoot {
     public static int floorSqrt(int x) {
         if (x == 0 || x == 1) return x;
-
         int start = 1, end = x, ans = 0;
         while (start <= end) {
             int mid = (start + end) / 2;
